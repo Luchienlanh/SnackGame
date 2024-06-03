effb288e3ab81dfd008452940ccd04ae415789ee
NguyenMinh <157773105+Oxazolidinone@users.noreply.github.com>
2024-06-03 10:25:29 +0700

    Update Setting.cpp

--- Setting.cpp
+++ Setting.cpp
@@ -1,20 +1,35 @@
 void settings(){
 	system("cls");
+	setConsoleColour(consoleforeground::DARKYELLOW);
 	int a=10,i,j;
 	cout<<"\nEnter your choice seperated by spaces and press enter (1 1 -> changes mode to colour)\n\n";
+	setConsoleColour(consoleforeground::DARKYELLOW);
 	cout<<"1. Choose Mode-\n";
+	setConsoleColour(consoleforeground::GRAY);
 	cout<<"1. Colour    2. Characters \n\n";
+	setConsoleColour(consoleforeground::DARKYELLOW);
 	cout<<"2. Choose Difficulty";
+	setConsoleColour(consoleforeground::GRAY);
 	cout<<"\n1. Easy      2. Medium         3. Hard      4. Custom (enter time in millisecons)     \n\n";
+	setConsoleColour(consoleforeground::DARKYELLOW);
 	cout<<"3. Want to pass through walls";
+	setConsoleColour(consoleforeground::GRAY);
 	cout<<"\n1. Yes       2. No\n\n";
+	setConsoleColour(consoleforeground::DARKYELLOW);
 	cout<<"4. Set the dimensions of BOARD \n";
+	setConsoleColour(consoleforeground::GRAY);
 	cout<<"Enter height and width- '25 100'    If there is problem in printing then try decreasing the size, Default-27 118\n\n";
+	setConsoleColour(consoleforeground::DARKYELLOW);
 	cout<<"5. Die by hitting your own body\n";
+	setConsoleColour(consoleforeground::GRAY);
 	cout<<"1. Yes       2. No\n\n";
+	setConsoleColour(consoleforeground::DARKYELLOW);
 	cout<<"6. Do you want to go reverse (if allowed then snake will not die by touching its own body)\n";
+	setConsoleColour(consoleforeground::GRAY);
 	cout<<"1. Yes       2. No\n\n";
+	setConsoleColour(consoleforeground::DARKYELLOW);
 	cout<<"7. Back\n\n";
+	setConsoleColour(consoleforeground::WHITE);
 	while(a!=7){
 		cin>>a;
 		if(a==1){
